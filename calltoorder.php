6cd22380774b507c7247d2afee548f841ac46620
A111ex <MasterMiMiKi@gmail.com>
2015-11-22 23:17:04 +0300

    2 commit 15-11-2015

---
+++ calltoorder.php
@@ -0,0 +1,50 @@
+<?php
+
+header('Content-Type: text/html; charset=utf-8');
+
+if (isset($_POST['namecall'])) {
+    $namecall = $_POST['namecall'];
+}
+if (isset($_POST['telcall'])) {
+    $telcall = $_POST['telcall'];
+}
+
+if (isset($namecall) && isset($telcall)) {
+    if ($namecall) {
+        trim($namecall);
+    } else {
+        $namecall = "";
+    }
+    if ($telcall) {
+        trim($telcall);
+    } else {
+        $telcall = "";
+    }
+
+    $mail = "MasterV12@ya.ru, vic@victorial.biz, advokatkrasnodar@yandex.ru";
+//    $mail = "advokatkrasnodar@yandex.ru, vic@victorial.biz";
+
+    $title_mass = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=\"utf-8\"\r\n";
+    $title_mass .= "From: info@advokatkrasnodar.com\r\n";
+    if ($_POST['metrika'] == 'akcia') {
+        $title = "Акция - заявка.";
+    } elseif ($_POST['metrika'] == 'zayavka') {
+        $title = "Оставить  - заявку.";
+    } elseif ($_POST['metrika'] == 'is_quest') {
+        $title = "Остались вопросы.";
+    } else {
+        $title = "Заказан обратный звонок.";
+    }
+    $title .= ' ' . $_SERVER['HTTP_REFERER'];
+    $message = "\nБесплатная консультация!!!\nФИО: " . $namecall . "\nТелефон: " . $telcall;
+    if ($_POST['metrika'] == 'vopros') {
+        $emailcall = ( isset($_POST['emailcall']) ? $_POST['emailcall'] : '' );
+        $messagecall = ( isset($_POST['messagecall']) ? $_POST['messagecall'] : '' );
+        $message .= "E-mail: " . $emailcall . "<br/>Текст : " . $messagecall;
+    }
+//    sand mail
+    mail($mail, $title, $message, $title_mass);
+} else {
+    exit("Вы не туда зашли");
+}
+?>
---
+++ js/main.js
@@ -0,0 +1,77 @@
+var metrika = '';
+$(function () {
+    var telcheck = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/i;
+    //console.log(12345);
+    $("a.fancybox").fancybox({});
+    $(".fancy-form").fancybox({padding: 0});
+
+    $('.phone').inputmask("mask", {"mask": "+7(999) 999-99-99", 'placeholder': 'x'});
+
+
+
+
+    $('.callbutton').click(function () {
+        var name_field = $(this).parent().find('[name="name"]');
+        var phone_field = $(this).parent().find('[name="phone"]');
+
+        var namecall = $(name_field).val();
+        var telcall = $(phone_field).val();
+        $(name_field).css({background: ''});
+        if (namecall == "") {
+            $(name_field).css({background: '#fec7c7'});
+            return false;
+        }
+        else if (telcall == "" || !telcheck.test(telcall)) {
+            $(phone_field).css({background: '#fec7c7'});
+            return false;
+        } else {
+            var dataform = {namecall: namecall, telcall: telcall, metrika: metrika};
+
+            $.ajax({
+                url: 'calltoorder.php',
+                type: "POST",
+                data: dataform,
+                dataType: "html",
+                success: function () {
+                    //console.log(datalog);
+                    //yaCounter31269228.reachGoal(metrika);
+                    $.fancybox({'href': 'img/call-ok.png', 'autoDimensions': false, 'height': 'auto', padding: 0});
+                },
+                error: function () {
+                    alert("Ошибка, Что то не то");
+                }
+            }).responseText;
+
+            $(name_field).val("").css({background: ''});
+            $(phone_field).val("").css({background: ''});
+        }
+        return false;
+    });
+
+    $("#accordion").accordion({
+        collapsible: true,
+        heightStyle: "content",
+        active: false
+    });
+
+$('.slider1').bxSlider({
+    slideWidth: 225,
+    minSlides: 4,
+    maxSlides: 4,
+    onSliderLoad: function(){
+        $('.bx-controls').hide();
+    }
+  });
+
+
+
+$('.arl').click(function (){
+    $('.bx-prev').click();
+});
+$('.arr').click(function (){
+    $('.bx-next').click();
+});
+
+});
+
+
